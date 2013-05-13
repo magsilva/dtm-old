8570ef8a93d3a3aa45ea7740b8d28d5755fbd5e6
Marco Aurélio Graciotto Silva <magsilva@gmail.com>
2013-05-13 00:51:09 -0300

    Add a helper script to cross-compile DTM. For now, we can cross-compile to Linux 32 and 64 bits. The script should probably work for creating files to Windows and Darwin (MacOS X), but the local machine does not have suitable versions of GSL and GFLAGS libraries for those platforms.

---
+++ dtm/build.sh
@@ -0,0 +1,51 @@
+# make \
+#	NM=x86_64-w64-mingw32-nm \
+#	RANLIB=x86_64-w64-mingw32-ranlib \
+#	CC=x86_64-w64-mingw32-gcc \
+#	CCC=x86_64-w64-mingw32-g++ \
+#	CXX=x86_64-w64-mingw32-g++ \
+#	AS=x86_64-w64-mingw32-as \
+#	OUTPUT_FILE="dtm-win32"
+
+# make \
+#	NM=i686-w64-mingw32-nm \
+#	RANLIB=i686-w64-mingw32-ranlib \
+#	CC=i686-w64-mingw32-gcc \
+#	CCC=i686-w64-mingw32-g++ \
+#	CXX=i686-w64-mingw32-g++ \
+#	AS=i686-w64-mingw32-as \
+#	OUTPUT_FILE="dtm-win64"
+
+make clean
+make \
+	NM=nm \
+	RANLIB=ranlib \
+	CC=gcc \
+	CCC=g++ \
+	CXX=g++ \
+	AS=as \
+	CFLAGS="-m32" \
+	CXXFLAGS="-m32" \
+	OUTPUT_FILE="dtm-linux32"
+
+make clean
+make \
+	NM=nm \
+	RANLIB=ranlib \
+	CC=gcc \
+	CCC=g++ \
+	CXX=g++ \
+	AS=as \
+	OUTPUT_FILE="dtm-linux64"
+
+make clean
+make \
+	CFLAGS="-arch x86_64" \
+	LDFLAGS="-arch x86_64" \
+	NM=darwinx-nm \
+	RANLIB=darwinx-ranlib \
+	CC=darwinx-gcc \
+	CCC=darwinx-g++ \
+	CXX=darwinx-g++ \
+	AS=darwinx-as \
+	OUTPUT_FILE="dtm-darwin64"
