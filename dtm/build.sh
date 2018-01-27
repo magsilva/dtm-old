3434d5553d2d0986a9049f0eb2b05765cb3cd564
Marco Aurélio Graciotto Silva <magsilva@utfpr.edu.br>
2018-01-27 10:22:20 -0200

    Improve building of Windows binaries (and disable compilation for Darwin/MacOS for now).

--- dtm/build.sh
+++ dtm/build.sh
@@ -1,22 +1,26 @@
 make clean
-make main-static \
-	NM=x86_64-w64-mingw32-nm \
-	RANLIB=x86_64-w64-mingw32-ranlib \
+PATH=/usr/x86_64-w64-mingw32/bin:$PATH make main \
+	NM=nm \
+	RANLIB=ranlib \
 	CC=x86_64-w64-mingw32-gcc \
 	CCC=x86_64-w64-mingw32-g++ \
 	CXX=x86_64-w64-mingw32-g++ \
-	AS=x86_64-w64-mingw32-as \
-	OUTPUT_FILE="dtm-win32.exe"
+	AS=as \
+	OPTFLAGS="-O2 -static" \
+	EXTRA_LDFLAGS="-lshlwapi" \
+	OUTPUT_FILE="dtm-win64.exe"
 
 make clean
-make main-static \
-	NM=i686-w64-mingw32-nm \
-	RANLIB=i686-w64-mingw32-ranlib \
+PATH=/usr/i686-w64-mingw32/bin:$PATH make main \
+	NM=nm \
+	RANLIB=ranlib \
 	CC=i686-w64-mingw32-gcc \
 	CCC=i686-w64-mingw32-g++ \
 	CXX=i686-w64-mingw32-g++ \
-	AS=i686-w64-mingw32-as \
-	OUTPUT_FILE="dtm-win64.exe"
+	AS=as \
+	OPTFLAGS="-O2 -static" \
+	EXTRA_LDFLAGS="-lshlwapi" \
+	OUTPUT_FILE="dtm-win32.exe"
 
 make clean
 make main \
@@ -40,14 +44,16 @@ make main \
 	AS=as \
 	OUTPUT_FILE="dtm-linux64"
 
-make clean
-make main \
-	CFLAGS="-arch x86_64" \
-	LDFLAGS="-arch x86_64" \
-	NM=darwinx-nm \
-	RANLIB=darwinx-ranlib \
-	CC=darwinx-gcc \
-	CCC=darwinx-g++ \
-	CXX=darwinx-g++ \
-	AS=darwinx-as \
-	OUTPUT_FILE="dtm-darwin64"
+# make clean
+# make main \
+#	CFLAGS="-arch x86_64" \
+#	LDFLAGS="-arch x86_64" \
+#	EXTRA_INCLUDE_FLAGS="-I /usr/darwinx/usr/include/ 
+#	EXTRA_LD_FLAGS="-L /usr/darwinx/usr/lib"
+#	NM=darwinx-nm \
+#	RANLIB=darwinx-ranlib \
+#	CC=darwinx-gcc \
+#	CCC=darwinx-g++ \
+#	CXX=darwinx-g++ \
+#	AS=darwinx-as \
+#	OUTPUT_FILE="dtm-darwin64"
